c149c3b7d13d2b562e7a46a729f9903e72842da9
Erik Ouchterlony <erik.ouchterlony@bredband.net>
2015-01-30 22:43:53 +0100

    add async test

--- src/leiningen/new/reagent_liveedit/core_test.cljs
+++ src/leiningen/new/reagent_liveedit/core_test.cljs
@@ -1,6 +1,6 @@
 (ns {{ns-name}}.core-test
   (:require-macros [{{ns-name}}.test-macros :refer [defdomtest defdom]])
-  (:require [cemerick.cljs.test :as t :refer-macros [is deftest]]
+  (:require [cemerick.cljs.test :as t :refer-macros [is deftest done]]
             [dommy.core         :as dommy :refer-macros [sel sel1]]
             [reagent.core       :as r]
             [{{ns-name}}.core        :as {{ns-name}}]
@@ -15,6 +15,14 @@
 (deftest trivial-error
   #_(is (1 = 1) "Don't do this!!!"))
 
+(deftest ^:async trivial-async-test
+  (let [now #(.getTime (js/Date.))
+        t (now)]
+    (js/setTimeout
+      (fn []
+        (is (>= (now) (+ t 10)))
+        (done))
+      10)))
 
 (defdomtest page1 {[:current-page] :page1}
   (is (= (some-> [:#main :h2] sel1 dommy/text)
