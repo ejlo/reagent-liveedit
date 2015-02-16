fa7a35c17b58de61fe5504d292ebeaf58b993955
Erik Ouchterlony <erik.ouchterlony@bredband.net>
2015-02-16 10:51:32 +0100

    version 0.1.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reagent-liveedit/lein-template "0.1.4"
+(defproject reagent-liveedit/lein-template "0.1.5-SNAPSHOT"
   :description "A leiningen template based on reagent-template for live editing a clojurescript web app"
   :url "https://github.com/ejlo/reagent-liveedit/"
   :license {:name "MIT License"
